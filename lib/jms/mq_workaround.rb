222c022391986ff1eec03813b0d396d450b0cd3a
Doug Everly <Doug@Everly.org>
2015-06-09 17:13:25 -0400

    Add MQTopicSession and MQTopicSubscriber support for IBM MQ JMS implementation

--- lib/jms/mq_workaround.rb
+++ lib/jms/mq_workaround.rb
@@ -64,6 +64,32 @@ begin
     end
     
   end
+  
+  class com.ibm.mq.jms::MQTopicSession
+
+    if self.method_defined? :consume
+      def consume(params, &proc)
+        Java::JavaxJms::Session.instance_method(:consume).bind(self).call(params, &proc)
+      end
+    end
+
+  end
+  
+  class com.ibm.mq.jms::MQTopicSubscriber
+
+    if self.method_defined? :each
+      def each(params, &proc)
+        Java::JavaxJms::MessageConsumer.instance_method(:each).bind(self).call(params, &proc)
+      end
+    end
+
+    if self.method_defined? :get
+      def get(params={})
+        Java::JavaxJms::MessageConsumer.instance_method(:get).bind(self).call(params)
+      end
+    end
+
+  end
 
 rescue NameError
   # Ignore errors (when we aren't using MQ)
